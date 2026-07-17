22474aca9ee7d840a816606c9112f3b3f4a757b0
eric1sphang-svg <eric1s.phang@gmail.com>
2026-07-17 16:18:57 +0700

    information-schedule

--- components/data-template.ts
+++ components/data-template.ts
@@ -27,23 +27,23 @@ export const scheduleDays: ScheduleDay[] = [
   {
     index: 1,
     judul: "INDEPENDENCE DAY",
-    title: "UPACARA BENDERA & OPENING CEREMONY",
+    title: "UPACARA BENDERA & OPENING CEREMONY & SEMINAR 5E & TM SELLING",
     date: "Minggu, 17 Agustus 2026",
-    location: "UC Plaza, Universitas Ciputra Surabaya",
-    time: ["06.30 - 08.05 WIB (Upacara Bendera)", "08.05 - 09:48 WIB (Opening Ceremony)"]
+    location: "Universitas Ciputra Surabaya",
+    time: ["06.30 - 08.05 WIB (Upacara Bendera)", "08.05 - 09:48 WIB (Opening Ceremony)", "09:48 - 13.32 WIB (Seminar 5E)", "14:20 - 16.47 WIB (TM SELLING)"]
   },
   {
     index: 2,
     judul: "DAY 1",
-    title: "WE SEE THE LIGHT (TALKSHOW)",
+    title: "WE SEE THE LIGHT (TALKSHOW & SHOWCASE SELLING)",
     date: "Senin, 31 Agustus 2026",
     location: "Hall Sekolah Ciputra Surabaya",
-    time: ["Sesi 1 7.30-12.15 WIB (Kelompok 1-67)", "Sesi 2 12.15-17.00 WIB (Kelompok 68-133)"],
+    time: ["Sesi 1 7.30-12.15 WIB", "Sesi 2 12.15-17.00 WIB"],
   },
   {
     index: 3,
     judul: "DAY 2",
-    title: "WHERE MAGIC FINDS YOU (SDP + ORMAWA FEST)",
+    title: "WHERE MAGIC FINDS YOU (STUDENT DEVELOPMENT PROGRAM & ORMAWA FEST)",
     date: "Selasa, 1 September 2026",
     location: "Universitas Ciputra Surabaya",
     time: "07.00 - 17.00 WIB",
@@ -51,7 +51,7 @@ export const scheduleDays: ScheduleDay[] = [
   {
     index: 4,
     judul: "DAY 3",
-    title: "INTO THE EVEN AFTER (PRA-LDK + ORMAWA)",
+    title: "INTO THE EVER AFTER (PRA-LDK & ORMAWA FEST)",
     date: "Rabu, 2 September 2026",
     location: "Universitas Ciputra Surabaya",
     time: "07.00 - 15.30 WIB",
@@ -62,14 +62,14 @@ export const scheduleDays: ScheduleDay[] = [
     title: "SYMBIOTIC PALLETE FOR INFINITE FLAVOURS (SELLING DAY)",
     date: "Kamis, 3 September 2026",
     location: "Ciputra World Mall Surabaya",
-    time: "07.00 - 12.00 WIB",
+    time: "08.30 - 21.00 WIB",
   },
   {
     index: 6,
     judul: "DAY 5",
-    title: "FLAME OF TRIUMPH  (SIDANG SENAT & CLOSING)",
-    date: "Sabtu, 30 Agustus 2026 ",
-    location: "Palimanan Resto & Cafe",
-    time: "14.20 - 21.45 WIB",
+    title: "THE GOLDEN EPILOGUE  (SIDANG SENAT & CLOSING)",
+    date: "Jumat, 4 September 2026 ",
+    location: "Ciputra World Mall Surabaya",
+    time: "14.00 - 21.10 WIB",
   },
 ];
