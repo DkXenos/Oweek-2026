553b341fdfc4fb2352c1cbd551d90c04ad20d301
DkXenos <superjason101010@gmail.com>
2026-07-27 16:28:05 +0700

    data template updated dresscode

--- components/data-template.ts
+++ components/data-template.ts
@@ -30,7 +30,13 @@ export const scheduleDays: ScheduleDay[] = [
     title: "UPACARA BENDERA & OPENING CEREMONY & SEMINAR 5E & TM SELLING",
     date: "Minggu, 17 Agustus 2026",
     location: "Universitas Ciputra Surabaya",
-    time: ["06.30 - 08.05 WIB (Upacara Bendera)", "08.05 - 09:48 WIB (Opening Ceremony)", "09:48 - 13.32 WIB (Seminar 5E)", "14:20 - 16.47 WIB (TM SELLING)"]
+    time: [
+      "06.30 - 08.05 WIB (Upacara Bendera)",
+      "08.05 - 09:48 WIB (Opening Ceremony)",
+      "09:48 - 13.32 WIB (Seminar 5E)",
+      "14:20 - 16.47 WIB (TM SELLING)",
+      "Dresscode: Kemeja Putih, Celana Panjang Hitam Formal, Sepatu Hitam",
+    ],
   },
   {
     index: 2,
@@ -38,7 +44,11 @@ export const scheduleDays: ScheduleDay[] = [
     title: "WE SEE THE LIGHT (TALKSHOW & SHOWCASE SELLING)",
     date: "Senin, 31 Agustus 2026",
     location: "Hall Sekolah Ciputra Surabaya",
-    time: ["Sesi 1 7.30-12.15 WIB", "Sesi 2 12.15-17.00 WIB"],
+    time: [
+      "Sesi 1 7.30-12.15 WIB",
+      "Sesi 2 12.15-17.00 WIB",
+      "Dresscode: Kemeja/Blouse Putih, Celana Panjang Hitam Bebas, Sepatu Bebas (Menutupi Jari dan Tumit)",
+    ],
   },
   {
     index: 3,
@@ -46,7 +56,10 @@ export const scheduleDays: ScheduleDay[] = [
     title: "WHERE MAGIC FINDS YOU (STUDENT DEVELOPMENT PROGRAM & ORMAWA FEST)",
     date: "Selasa, 1 September 2026",
     location: "Universitas Ciputra Surabaya",
-    time: "07.00 - 17.00 WIB",
+    time: [
+      "07.00 - 17.00 WIB",
+      "Dresscode: Kemeja/Blouse Warna Bebas, Celana Panjang Bebas, Sepatu Bebas (Menutupi Jari dan Tumit)",
+    ],
   },
   {
     index: 4,
@@ -54,7 +67,10 @@ export const scheduleDays: ScheduleDay[] = [
     title: "INTO THE EVER AFTER (PRA-LDK & ORMAWA FEST)",
     date: "Rabu, 2 September 2026",
     location: "Universitas Ciputra Surabaya",
-    time: "07.00 - 15.30 WIB",
+    time: [
+      "07.00 - 15.30 WIB",
+      "Dresscode: Baju Batik Berkerah (DILARANG DIJADIKAN OUTER), Celana Panjang Bebas (Tidak Sobek), Sepatu Sneakers (Warna Bebas)",
+    ],
   },
   {
     index: 5,
@@ -62,7 +78,7 @@ export const scheduleDays: ScheduleDay[] = [
     title: "SYMBIOTIC PALLETE FOR INFINITE FLAVOURS (SELLING DAY)",
     date: "Kamis, 3 September 2026",
     location: "Ciputra World Mall Surabaya",
-    time: "08.30 - 21.00 WIB",
+    time: ["08.30 - 21.00 WIB", "Dresscode: Kaos Universitas Ciputra, celana panjang gelap tidak sobek, name tag, bersepatu (warna bebas) serta menggunakan Atribut/Aksesoris (Sesuai dengan Main Ingredient)"],
   },
   {
     index: 6,
@@ -70,6 +86,6 @@ export const scheduleDays: ScheduleDay[] = [
     title: "THE GOLDEN EPILOGUE  (SIDANG SENAT & CLOSING)",
     date: "Jumat, 4 September 2026 ",
     location: "Ciputra World Mall Surabaya",
-    time: "14.00 - 21.10 WIB",
+    time: ["14.00 - 21.10 WIB", "Dresscode Laki-Laki : Kemeja Putih, Celana Panjang Hitam, Sepatu Bebas", "Dresscode Perempuan : Kemeja Putih, Celana Panjang Hitam / Rok Hitam Panjang (dibawah lutut), Sepatu Bebas"],
   },
 ];
