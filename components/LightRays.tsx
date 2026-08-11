21ce6361e6466e13673d896941aac0f1a8af8c3f
DkXenos <superjason101010@gmail.com>
2026-08-11 21:25:11 +0700

    deployment test

--- components/LightRays.tsx
+++ components/LightRays.tsx
@@ -1,6 +1,6 @@
 import { useRef, useEffect, useState } from 'react';
 import { Renderer, Program, Triangle, Mesh } from 'ogl';
-
+//build test
 export type RaysOrigin =
   | 'top-center'
   | 'top-left'
